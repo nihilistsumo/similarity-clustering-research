20955ccce9193735f696a9e31b888fdd4d7155b7
shubham <shubham.chatterjee94@gmail.com>
2017-11-22 10:24:46 -0500

    Added classes to calculate semantic similarity
    Several methods to calculate semantic similarity between two paragraphs
    have been implemented.

--- src/main/java/edu/unh/cs/treccar/proj/SimilarityFunction.java
+++ src/main/java/edu/unh/cs/treccar/proj/SimilarityFunction.java
@@ -1,5 +1,9 @@
 package edu.unh.cs.treccar.proj;
 
+import java.util.ArrayList;
+
+import edu.unh.cs.treccar.Data;
+
 public interface SimilarityFunction {
-	public double simScore(ParaPair pp);
+	public double simScore(ParaPair pp, ArrayList<Data.Paragraph> list);
 }

b121617ecba9081ea50387014685df71ce5243b3
shubham <shubham.chatterjee94@gmail.com>
2017-11-22 10:11:59 -0500

    Semantic Similarity Methods
    Added several classes to find semantic similarity between two paragraphs
    using several methods.

---
+++ src/main/java/edu/unh/cs/treccar/proj/WuPalmerSimilarity.java
@@ -0,0 +1,66 @@
+package edu.unh.cs.treccar.proj;
+
+import java.util.ArrayList;
+import edu.unh.cs.treccar.Data;
+
+/**
+ * This class implements the JiangConrath Semantic Similarity measure 
+ * and returns the measure between two paragraphs.
+ * @author Shubham Chatterjee
+ *
+ */
+
+public class WuPalmerSimilarity implements SimilarityFunction
+{
+	private static ArrayList<String> paraText1;
+	private static ArrayList<String> paraText2;
+	private static ArrayList<Double> scores;
+	double score = 0.0d;
+	
+	/**
+	 * @param pp ParaPair A ParaPair object representing a pair of paragraphs
+	 * @param list ArrayList<Data.Paragraph> Conatins a list of Data.Paragraph objects 
+	 * @return WuPalmer score between two paragraphs
+	 */
+	public double simScore(ParaPair pp, ArrayList<Data.Paragraph> list)
+	{
+		paraText1 = ParaUtilities.getParaText1(pp, list);
+		paraText2 = ParaUtilities.getParaText2(pp, list);
+		
+		score = getParaScore(paraText1, paraText2);
+		
+		return score;
+	}
+	/**
+	 * 
+	 * @param list1 ArrayList<String> List of words from first paragraph
+	 * @param list2 ArrayList<String> List of words from second paragraph
+	 * @return similarity score between two lists
+	 */
+	
+	private static double getParaScore(ArrayList<String> list1, ArrayList<String> list2)
+	{
+		double s = 0.0d;
+		for(String w1 : list1)
+			for(String w2 : list2)
+				scores.add(WordSemanticSimilarity.findSimilarity(w1, w2, "wp"));
+		s = findMeanScore(scores);
+		return s;
+	}
+	/**
+	 * 
+	 * @param list ArrayList<Double> List of scores between words of two paragraphs
+	 * @return mean of the score
+	 */
+	
+	private static double findMeanScore(ArrayList<Double> list)
+	{
+		double total = 0.0d;
+		double mean = 0.0d;
+		for(double i : list )
+			total += i;
+		mean = total/list.size();
+		return mean;
+	}
+}
+

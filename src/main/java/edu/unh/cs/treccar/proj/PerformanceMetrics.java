29791036011ba9043f5df2ede139a04290926d5e
sk1105 <sk1105@sk1105-XPS-8910>
2017-11-27 16:46:08 -0500

    adjusted rand index added

--- src/main/java/edu/unh/cs/treccar/proj/PerformanceMetrics.java
+++ src/main/java/edu/unh/cs/treccar/proj/PerformanceMetrics.java
@@ -42,4 +42,105 @@ public class PerformanceMetrics {
 		score = (double)pos/(double)count;
 		return score;
 	}
+	
+	public double calculateRandIndex(ArrayList<ArrayList<String>> candidateClusters, ArrayList<ArrayList<String>> gtClusters){
+		//candidateClusters will be map between cluster labels and cluster of para IDs
+		//String resultString = "";
+		//HashMap<String, ArrayList<String>> correct = this.groundTruth;
+		//HashMap<AssignParagraphs.SectionPathID, ArrayList<Data.Paragraph>> candidate = this.candidateAssign;
+		//String[] correctSections = new String[correct.size()];
+		//String[] candLabels = new String[candidateClusters.size()];
+		//correct.keySet().toArray(correctSections);
+		//candidateClusters.keySet().toArray(candLabels);
+		
+		int[][] contingencyMatrix = new int[gtClusters.size()][candidateClusters.size()];
+		double randIndex = 0.0;
+		ArrayList<String> correctParas = new ArrayList<String>();
+		ArrayList<String> candParas = new ArrayList<String>();
+		for(int i=0; i<gtClusters.size(); i++){
+			for(int j=0; j<candidateClusters.size(); j++){
+				int matchCount = 0;
+				correctParas = gtClusters.get(i);
+				candParas = candidateClusters.get(j);
+				if(correctParas == null){
+					System.out.println("We have null in correctParas!");
+				} else if(candParas != null){
+					for(String candPara : candParas){
+						if(correctParas.contains(candPara)){
+							matchCount++;
+						}
+					}
+				}
+				contingencyMatrix[i][j] = matchCount;
+			}
+		}
+		printContingencyMatrix(contingencyMatrix);
+		if((new Double(this.computeRand(contingencyMatrix))).isNaN()) {
+			System.out.println("Adjusted Rand index could not be computed!");
+			return -99;
+		}
+		else {
+			randIndex = this.computeRand(contingencyMatrix);
+			System.out.println("Calculated RAND index: "+randIndex);
+			return randIndex;
+		}
+	}
+	
+	private double computeRand(int[][] contMat){
+		double score = 0.0;
+		int sumnij=0, sumni=0, sumnj=0, nC2=0, nrow=0, ncol=0;		
+		ncol = contMat[0].length;
+		nrow = contMat.length;
+		int[] njvals = new int[ncol];
+		nC2 = this.nC2(ncol+nrow);
+		for(int i=0; i<nrow; i++){
+			int ni=0;
+			for(int j=0; j<ncol; j++){
+				sumnij+=this.nC2(contMat[i][j]);
+				ni+=contMat[i][j];
+				njvals[j]+=contMat[i][j];
+			}
+			sumni+=this.nC2(ni);
+		}
+		for(int j=0; j<njvals.length; j++){
+			sumnj+=this.nC2(njvals[j]);
+		}
+		
+		/* ################### 
+		 * This code is for simple Rand index
+		
+		int a=0, b=0, c=0, d=0;
+		a = sumnij;
+		b = sumni - sumnij;
+		c = sumnj - sumnij;
+		d = nC2 - (a+b+c);
+		score = ((double)(a+d))/(a+b+c+d);
+		
+		#################### */
+		
+		double denom = ((double)(sumni+sumnj))/2-((double)sumni*sumnj/nC2);
+		double nom = (sumnij-((double)(sumni*sumnj))/nC2);
+		System.out.println("sumnij: "+sumnij+", sumni: "+sumni+", sumnj: "+sumnj+", nC2: "+nC2+", nom: "+nom+", denom: "+denom);
+		score = nom/denom;
+		return score;
+	}
+	
+	private int nC2(int n){
+		if(n<2) return 0;
+		else if(n==2) return 1;
+		else{
+			return n*(n-1)/2;
+		}
+	}
+	
+	private void printContingencyMatrix(int[][] contingency){
+		int colNum = contingency[0].length;
+		int rowNum = contingency.length;
+		for(int i=0; i<rowNum; i++){
+			for(int j=0; j<colNum; j++){
+				System.out.print(contingency[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
 }

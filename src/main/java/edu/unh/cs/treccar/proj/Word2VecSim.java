3c8a25ae665c49145085c9dc58a57941ec15e9cf
nihilistsumo <kashyapisumanta@gmail.com>
2017-12-13 20:31:02 -0500

    pom issue fixed

--- src/main/java/edu/unh/cs/treccar/proj/Word2VecSim.java
+++ src/main/java/edu/unh/cs/treccar/proj/Word2VecSim.java
@@ -1,9 +1,13 @@
 package edu.unh.cs.treccar.proj;
 
+import java.util.ArrayList;
+
+import edu.unh.cs.treccar.Data;
+
 public class Word2VecSim implements SimilarityFunction {
 
 	@Override
-	public double simScore(ParaPair pp) {
+	public double simScore(ParaPair pp, ArrayList<Data.Paragraph> paralist) {
 		// TODO Auto-generated method stub
 		return 0;
 	}

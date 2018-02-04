76e1d0b40256a36efe3f6bf4d2efb4c7643a561c
nihilistsumo <kashyapisumanta@gmail.com>
2018-02-04 13:32:14 -0500

    Update SimilarityFunction.java
    SimilarityFunction same as script_cmd

--- src/main/java/edu/unh/cs/treccar/proj/SimilarityFunction.java
+++ src/main/java/edu/unh/cs/treccar/proj/SimilarityFunction.java
@@ -1,9 +1,11 @@
-package edu.unh.cs.treccar.proj;
+package edu.unh.cs.treccar.proj.similarities;
 
 import java.util.ArrayList;
 
+import edu.cmu.lti.lexical_db.ILexicalDatabase;
 import edu.unh.cs.treccar.Data;
+import edu.unh.cs.treccar.proj.util.ParaPair;
 
 public interface SimilarityFunction {
-	public double simScore(ParaPair pp, ArrayList<Data.Paragraph> list);
+	public double simScore(ParaPair pp, ILexicalDatabase db);
 }
